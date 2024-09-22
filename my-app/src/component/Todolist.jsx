121d1ecb0ab0ecf073036395ce6aa8d628452a2f
bahirafe <fekadubahiru0@gmail.com>
2024-09-22 07:13:41 -0700

    add some code

--- my-app/src/component/Todolist.jsx
+++ my-app/src/component/Todolist.jsx
@@ -17,19 +17,27 @@ setInputvalue('')
 setEditIndex(-1)
 }
 else{
-    setTodos([...todos,{task:inputvalue, completed:false}])
+    setTodos([...todos,{task :inputvalue, completed:false}])
     setInputvalue('')
 }
     }
 }
 
-const startEdit=(index)=>{
+const startEdit=(index) => {
     setInputvalue(todos[index].task)
     setEditIndex(index)
 }
 const cancelEdit=()=>{
     setInputvalue('')
     setEditIndex(-1)
+}
+const removeTodo=(index)=>{
+    const updateTodos=todos.filter((_, i)=> i !==index)
+    setTodos(updateTodos)
+}
+const toggleCompleted=(index)=>{
+    const updateTodos=[...todos]
+    updateTodos[index].completed=!updateTodos[index].completed
 }
     return (
         <div className="todo-container"><h2>To-Do List </h2>
@@ -41,18 +49,29 @@ const cancelEdit=()=>{
 
              
             <>
-            <button  className="update -btn"> <BiCheckDouble/></button>
-            <button  className="cancel -btn"><BiRefresh></BiRefresh></button>
+            <button onClick={addTodo} className="update -btn"> <BiCheckDouble/></button>
+            <button onClick={cancelEdit} className="cancel -btn"><BiRefresh></BiRefresh></button>
            
             </>
              ) :(
 
              
-            <button className="add-btn">Add</button>
+            <button onClick={addTodo} className="add-btn">Add</button>
              )}
         </div>
         <ul className="todo-list">
-
+            {todos.map((todo, index)=>(
+                <li key={index} className={todo.completed ? "completed" :""}>
+                    {todo.task}
+                    <div className="btn-group">
+                        <button onClick={startEdit} className="btn-edit"><BiEdit/></button>
+                        <button onCanPlay={removeTodo} className="btn-remove"><BiTrash></BiTrash></button>
+                        <button className="btn-done" onClick={()=> toggleCompleted}>
+                            {todo.completed ? <BiReset></BiReset> :<BiCheckCircle></BiCheckCircle>}
+                        </button>
+                    </div>
+                </li>
+            ))}
         </ul>
         </div>
     )
